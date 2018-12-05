4379d400ae0079d9b1b945d260b2dbe9accd89aa
Gayathri <gayathri@Gayathris-MacBook-Pro.local>
2018-12-04 16:36:17 -0800

    changed naming and colors

--- src/js/isOddOrEven.js
+++ src/js/isOddOrEven.js
@@ -1,10 +1,10 @@
 function isOddOrEven() {
-var number = parseFloat(document.getElementById("num1").value); 
+var number = parseFloat(document.getElementById("num").value);
   if (number%2 == 0) {
-    document.getElementById("int").innerHTML = number + ' is an Even number'; 
+    document.getElementById("result").innerHTML = number + ' is an Even number'; 
   } else if (number%2 == 1 || number%2 == -1 ) {
-    document.getElementById("int").innerHTML = number + ' is an Odd number';
+    document.getElementById("result").innerHTML = number + ' is an Odd number';
   } else {
-    document.getElementById("int").innerHTML = number + ' is a floating number';
+    document.getElementById("result").innerHTML = number + ' is a floating number';
   }
 }
