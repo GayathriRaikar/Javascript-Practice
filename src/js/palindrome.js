7b38ec7941c71dc79f63ba903bc9588fb0787842
Gayathri <gayathri@Gayathris-MacBook-Pro.local>
2018-12-18 16:42:17 -0800

    removed repition of code

--- src/js/palindrome.js
+++ src/js/palindrome.js
@@ -1,6 +1,6 @@
 var isPalindrome = function (userValue) {
   var userValue = document.getElementById("palindromeText").value;
-  var str = userValue.toLowerCase().replace(/[\W_]/g,'').toLowerCase();
+  var str = userValue.toLowerCase().replace(/[\W_]/g,'');
 
   if (str === str.split('').reverse().join('') || str.length === 1) {
     document.getElementById("result").innerHTML = str + " is a palindrome";
